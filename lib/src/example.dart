d4d626ff652ad8de0eb803d913201319945ce2fb
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-05-26 14:49:11 +0800

    Simple generation

---
+++ lib/src/example.dart
@@ -0,0 +1,28 @@
+
+part 'example.g.dart';
+
+const myStr = 'Lorem';
+final myInt = 12;
+final myDouble = 2.5;
+var myBool = false;
+final myList = [2];
+final myMap = {'a': 'b'};
+final mySet = {'a', 'b'}; // Set()..add('a')..add('b')
+final mySymbol = #g; // Symbol('g')
+
+void myFunc = () {
+  print('Hello world!!!');
+};
+
+class Order {
+  final int id;
+  final String name;
+  final String price;
+
+  const Order(
+    this.id,
+    this.name,
+    this.price,
+  );
+
+}
---
+++ lib/src/example.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// InfoGenerator
+// **************************************************************************
+
+/* (String get myStr, int get myInt, double get myDouble, ..., void myFunc, class Order) */
