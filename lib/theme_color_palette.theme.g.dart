afc0877a8c760533fe9803cdaa1c0bc770d83017
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-05-31 23:22:32 +0800

    :sparkles: Instances of specific collection

--- lib/theme_color_palette.theme.g.dart
+++ lib/theme_color_palette.theme.g.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+
 class ThemeColorPalette {
-	const ThemeColorPalette();
+	const ThemeColorPalette.dark(): textEmphasis = const TextEmphasis.dark();
+	const ThemeColorPalette.light(): textEmphasis = const TextEmphasis.light();
+	const ThemeColorPalette.grey(): textEmphasis = const TextEmphasis.grey();
 
 	static const colorPalette = ThemeColorPalette$ColorPalette();
+	final TextEmphasis textEmphasis;
 }
 
 class ThemeColorPalette$ColorPalette {
 	const ThemeColorPalette$ColorPalette();
 
-	static const white = Color(0xffffffff);
-	static const black = Color(0xff000000);
-	static const red = Color(0xffff0000);
-	static const nestedColorPalette = ThemeColorPalette$NestedColorPalette();
+	static const white = const Color(0xffffffff);
+	static const black = const Color(0xff000000);
+	static const red = const Color(0xffff0000);
+	static const nestedColorPalette = ThemeColorPalette$ColorPalette$NestedColorPalette();
 }
 
+class ThemeColorPalette$ColorPalette$NestedColorPalette {
+	const ThemeColorPalette$ColorPalette$NestedColorPalette();
 
-
-
-class ThemeColorPalette$NestedColorPalette {
-	const ThemeColorPalette$NestedColorPalette();
-
-	static const white = Color(0xff04ffff);
-	static const black = Color(0xff009300);
+	static const white = const Color(0xff04ffff);
+	static const black = const Color(0xff009300);
 }
 
+class TextEmphasis {
 
-
-
-
+	const TextEmphasis.dark(): white = const Color(0xff0f0fff), black = const Color(0xff89503d);
+	const TextEmphasis.light(): white = const Color(0xff0f0fff), black = const Color(0xff89503d);
+	const TextEmphasis.grey(): white = const Color(0xff0f0fff), black = const Color(0xff89503d);
+	final Color white;
+	final Color black;
+}
