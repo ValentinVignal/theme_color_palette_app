243cf134134631d15afd68eb330316ea054035b7
ValentinVignal <valentin.vignal.dev@outlook.fr>
2021-07-23 23:10:57 +0800

    :lipstick: Finish color palette

--- lib/theme_color_palette.theme.g.dart
+++ lib/theme_color_palette.theme.g.dart
@@ -15,7 +15,13 @@ class ThemeColorPalette {
 		iconEmphasis = const ThemeColorPalette$IconEmphasis.light(),
 		border = const ThemeColorPalette$Border.light(),
 		background = const ThemeColorPalette$Background.light(),
-		floatingActionButton = const ThemeColorPalette$FloatingActionButton.light();
+		floatingActionButton = const ThemeColorPalette$FloatingActionButton.light(),
+		task = const ThemeColorPalette$Task.light(),
+		form = const ThemeColorPalette$Form.light(),
+		notification = const ThemeColorPalette$Notification.light(),
+		projectTimeline = const ThemeColorPalette$ProjectTimeline.light(),
+		pin = const ThemeColorPalette$Pin.light(),
+		cardColor = const Color(0xffffffff);
 	/// The Theme Color Palette
 	const ThemeColorPalette.dark():
 		accentColor = const Color(0xff699fcf),
@@ -25,7 +31,13 @@ class ThemeColorPalette {
 		iconEmphasis = const ThemeColorPalette$IconEmphasis.dark(),
 		border = const ThemeColorPalette$Border.dark(),
 		background = const ThemeColorPalette$Background.dark(),
-		floatingActionButton = const ThemeColorPalette$FloatingActionButton.dark();
+		floatingActionButton = const ThemeColorPalette$FloatingActionButton.dark(),
+		task = const ThemeColorPalette$Task.dark(),
+		form = const ThemeColorPalette$Form.dark(),
+		notification = const ThemeColorPalette$Notification.dark(),
+		projectTimeline = const ThemeColorPalette$ProjectTimeline.dark(),
+		pin = const ThemeColorPalette$Pin.dark(),
+		cardColor = const Color(0xff1e1e1e);
 
 	/// Default radius
 	static const radius = 4.0;
@@ -39,6 +51,14 @@ class ThemeColorPalette {
 	static const lowOpacity = 0.6;
 	/// Medium Opacity
 	static const disabledOpacity = 0.38;
+	/// High elevation
+	static const highElevation = 3.0;
+	/// Medium elevation
+	static const mediumElevation = 2.0;
+	/// Low elevation
+	static const lowElevation = 1.0;
+	/// None elevation
+	static const noneElevation = 0.0;
 
 	/// Accent color
 	@Deprecated('Use theme.accentColor instead')
@@ -58,6 +78,19 @@ class ThemeColorPalette {
 	final ThemeColorPalette$Background background;
 	/// Floating action button
 	final ThemeColorPalette$FloatingActionButton floatingActionButton;
+	/// 
+	final ThemeColorPalette$Task task;
+	/// Form color palette
+	final ThemeColorPalette$Form form;
+	/// Notification color palette
+	final ThemeColorPalette$Notification notification;
+	/// Project time line color palette
+	final ThemeColorPalette$ProjectTimeline projectTimeline;
+	/// Pin theme
+	final ThemeColorPalette$Pin pin;
+	/// Card Color
+	@Deprecated('Use theme.cardColor instead')
+	final Color cardColor;
 }
 
 
@@ -246,6 +279,134 @@ class ThemeColorPalette$FloatingActionButton {
 
 	/// Disabled color
 	final Color disabledColor;
+}
+
+
+// -------------------- ThemeColorPalette$Task --------------------
+
+/// 
+class ThemeColorPalette$Task {
+	/// 
+	const ThemeColorPalette$Task.light():
+		notStarted = const Color(0xffed6e19),
+		workInProgress = const Color(0xffeaa60f),
+		completed = const Color(0xff81bf48);
+	/// 
+	const ThemeColorPalette$Task.dark():
+		notStarted = const Color(0xffed6e19),
+		workInProgress = const Color(0xffeaa60f),
+		completed = const Color(0xff81bf48);
+
+	/// Not started color
+	final Color notStarted;
+	/// Work in progress color
+	final Color workInProgress;
+	/// Completed color
+	final Color completed;
+}
+
+
+// -------------------- ThemeColorPalette$Form --------------------
+
+/// Form color palette
+class ThemeColorPalette$Form {
+	/// Form color palette
+	const ThemeColorPalette$Form.light():
+		drat = const Color(0xffed6e19),
+		actionRequired = const Color(0xffeaa60f),
+		completed = const Color(0xff666666),
+		rejected = const Color(0xffc73826);
+	/// Form color palette
+	const ThemeColorPalette$Form.dark():
+		drat = const Color(0xffed6e19),
+		actionRequired = const Color(0xffeaa60f),
+		completed = const Color(0xff666666),
+		rejected = const Color(0xffc73826);
+
+	/// Draft color
+	final Color drat;
+	/// Action required color
+	final Color actionRequired;
+	/// Completed color
+	final Color completed;
+	/// Rejected color
+	final Color rejected;
+}
+
+
+// -------------------- ThemeColorPalette$Notification --------------------
+
+/// Notification color palette
+class ThemeColorPalette$Notification {
+	/// Notification color palette
+	const ThemeColorPalette$Notification.light():
+		countBubbleColor = const Color(0xfff26716);
+	/// Notification color palette
+	const ThemeColorPalette$Notification.dark():
+		countBubbleColor = const Color(0xfff26716);
+
+	/// Color of the notification bubble
+	final Color countBubbleColor;
+}
+
+
+// -------------------- ThemeColorPalette$ProjectTimeline --------------------
+
+/// Project time line color palette
+class ThemeColorPalette$ProjectTimeline {
+	/// Project time line color palette
+	const ThemeColorPalette$ProjectTimeline.light():
+		plannedLight = const Color(0xffeef3f8),
+		planned = const Color(0xff647e9b),
+		start = const Color(0xffeaa60f),
+		completed = const Color(0xff7ec141),
+		today = const Color(0xff2864ac),
+		active = const Color(0xffeef3f8),
+		progress = const Color(0xff2864ac);
+	/// Project time line color palette
+	const ThemeColorPalette$ProjectTimeline.dark():
+		plannedLight = const Color(0xffeef3f8),
+		planned = const Color(0xff647e9b),
+		start = const Color(0xffeaa60f),
+		completed = const Color(0xff7ec141),
+		today = const Color(0xffc1dbef),
+		active = const Color(0xffeef3f8),
+		progress = const Color(0xff2864ac);
+
+	/// Planned disc color
+	final Color plannedLight;
+	/// Planned text color
+	final Color planned;
+	/// Start color
+	final Color start;
+	/// Completed color
+	final Color completed;
+	/// Today disc color
+	final Color today;
+	/// Color for the Timeline Base
+	final Color active;
+	///  Color for the active part of the Timeline bar (Start -> Today/Completed)
+	final Color progress;
+}
+
+
+// -------------------- ThemeColorPalette$Pin --------------------
+
+/// Pin theme
+class ThemeColorPalette$Pin {
+	/// Pin theme
+	const ThemeColorPalette$Pin.light():
+		iconColor = const Color(0xffffffff),
+		disabledBackgroundColor = const Color(0xff8d8d8d);
+	/// Pin theme
+	const ThemeColorPalette$Pin.dark():
+		iconColor = const Color(0xffffffff),
+		disabledBackgroundColor = const Color(0xff8d8d8d);
+
+	/// Icon Color
+	final Color iconColor;
+	/// The background color when the pin is disabled
+	final Color disabledBackgroundColor;
 }
 
 
